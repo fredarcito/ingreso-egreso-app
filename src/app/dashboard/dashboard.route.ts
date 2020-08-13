b88a0244ecc8a055410a140b3851e652fa25068f
fredarcito <fredar21@gmail.com>
2020-08-13 11:17:47 -0500

    first commit

---
+++ src/app/dashboard/dashboard.route.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router'
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component'
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component'
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component'
+
+
+export const dashboardRoutes: Routes = [
+    { path: '', component: EstadisticaComponent },
+    { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+    { path: 'detalle', component: DetalleComponent },
+    
+];
